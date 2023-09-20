263ec15a9723c2fbbd6ae7fbf76d6c3c385ec798
Ziggy Chen <ziggycs9796@hotmail.com>
2023-09-20 21:05:41 +0800

    build: update-version

---
+++ scripts/update-version.ts
@@ -0,0 +1,46 @@
+import consola from 'consola'
+import chalk from 'chalk'
+import { errorAndExit, getWorkspacePackages } from '@vz-components/build-utils'
+import type { Project } from '@pnpm/find-workspace-packages'
+
+async function main() {
+  const tagVersion = process.env.TAG_VERSION
+  const gitHead = process.env.GIT_HEAD
+  if (!tagVersion || !gitHead) {
+    errorAndExit(
+      new Error(
+        'No tag version or git head were found, make sure that you set the environment variable $TAG_VERSION \n'
+      )
+    )
+  }
+
+  consola.log(chalk.cyan('Start updating version'))
+  consola.log(chalk.cyan(`$TAG_VERSION: ${tagVersion}`))
+  consola.log(chalk.cyan(`$GIT_HEAD: ${gitHead}`))
+
+  consola.debug(chalk.yellow(`Updating package.json for vz-components`))
+
+  const pkgs = Object.fromEntries(
+    (await getWorkspacePackages()).map((pkg) => [pkg.manifest.name!, pkg])
+  )
+  const vzComponents = pkgs['vz-components']
+
+  const writeVersion = async (project: Project) => {
+    await project.writeProjectManifest({
+      ...project.manifest,
+      version: tagVersion,
+      gitHead,
+    } as any)
+  }
+
+  try {
+    await writeVersion(vzComponents)
+  } catch (err: any) {
+    errorAndExit(err)
+  }
+
+  consola.debug(chalk.green(`$GIT_HEAD: ${gitHead}`))
+  consola.success(chalk.green(`Git head updated to ${gitHead}`))
+}
+
+main()
